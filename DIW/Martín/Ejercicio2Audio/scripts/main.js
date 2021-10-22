66e7123686c8d880440493d5684dd0f707bc8491
UrkoU <ikbqa@plaiaundi.net>
2021-10-22 13:05:37 +0200

    Work done on 22/10

--- DIW/Martín/Ejercicio2Audio/scripts/main.js
+++ DIW/Martín/Ejercicio2Audio/scripts/main.js
@@ -1,3 +1,28 @@
 document.querySelector("#audio").addEventListener("play", (ev) => {
   console.log("Playing", ev.target);
 });
+
+const songs = ["media/AC-DC.mp3", "media/Aretha.mp3", "media/Bob Marley.mp3", "media/Red Hot Chili Peppers.mp3"];
+const reproductor = document.getElementById("audio");
+let current = 0;
+
+function playAudio(num) {
+  current = num;
+  reproductor.src = songs[current];
+  reproductor.load();
+  reproductor.play();
+
+  // document.querySelector("#audio").querySelector("source").src = songs[num - 1];
+}
+
+document.querySelector("#control-previous").addEventListener("click", (ev) => {
+  ev.preventDefault();
+  playAudio(current === 0 ? (current = 3) : --current);
+  console.log("Previous", ev.target);
+});
+
+document.querySelector("#control-next").addEventListener("click", (ev) => {
+  ev.preventDefault();
+  playAudio(current === 3 ? (current = 0) : ++current);
+  console.log("Previous", ev.target);
+});
