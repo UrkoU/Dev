95d96cc4884b284a60917d3a70658e42251fa9db
UrkoU <ikbqa@plaiaundi.net>
2022-02-08 13:05:20 +0100

    Almost no work done in 8/2

--- DWC/DWCWebs/Basurero/JavaScriptPruebas/Ajax1/ActividadElecciones/GrabaVotos.php
+++ DWC/DWCWebs/Basurero/JavaScriptPruebas/Ajax1/ActividadElecciones/GrabaVotos.php
@@ -1,24 +1,32 @@
 <?php
-$partido = $_POST['Partido'];
-$votos = $_POST['Votos'];
-
+$partido=utf8_encode($_REQUEST["partido"]); 
+$votos=utf8_encode($_REQUEST["votos"]); 
 $servername = "localhost";
-//Conexi√≥nalabasededatos:
-$link = @new mysqli($servername, "root", "A.bc1234", "enrique");
+$username = "urko";
+$password = "urko";
+// Conexión a la base de datos:
+$link = @new mysqli($servername, $username, $password, "urko");
 
 if ($link->connect_errno) {
-    die('ConnectError:' . $link->connect_error);
+    die('Connect Error: ' . $link->connect_error);
 }
+// Consulta
+$sql = "UPDATE elecciones SET Votos = $votos WHERE Partido = $partido";
+// $resultado = $link->query($sql);
 
-if ($link->connect_errno) {
-    die('ConnectError:' . $link->connect_error);
+// for ($i=0;$i<$filas;$i++){
+//     $fila = mysqli_fetch_array($resultado);
+//     // Crear el array de objetos con ciudad y tiempo
+//     $miArray[$i] = array("Pais"=>$fila["Pais"], "Oro"=>$fila["Oro"], "Plata"=>$fila["Plata"], "Bronce"=>$fila["Bronce"]);
+// }
+// Control de si se ha realizado o no el update
+if ($link->query($sql) === TRUE) {
+    echo "'$sql' <a href='Votacion.html'>Volver</a>";
+}else {
+    echo "ERROR";
 }
-
-//Consulta
-$sql = "UPDATE elecciones SET Votos = $votos WHERE Partido = '$partido'";
-$resultado = $link->query($sql);
-
-echo "<p>UPDATE elecciones SET Votos = " . $votos . "WHERE Partido = '" . $partido . "'</p>";
-echo "<button><a href='GrabaVotos.html'>Volver</a></button>";
-
+// echo  `UPDATE elecciones SET Votos = $votos WHERE Partido = $partido`;
+// echo `<button onclick="console.log('here')">`
+// echo $votos;
 mysqli_close($link);
+?>
